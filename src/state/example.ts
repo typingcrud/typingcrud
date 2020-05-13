d3acad0d29305399e686b7114b8d9374c91f78f7
shimehituzi <shimehituzi@gmail.com>
2020-05-14 08:15:08 +0900

    test createAsyncThunk

--- src/state/example.ts
+++ src/state/example.ts
@@ -1,4 +1,4 @@
-import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+import { createSlice, PayloadAction, createAsyncThunk } from '@reduxjs/toolkit'
 import { AppThunk } from 'state'
 
 type Example = {
@@ -9,6 +9,14 @@ const initialState: Example = {
   text: 'example'
 }
 
+export const setExampleThunk = createAsyncThunk<Example>(
+  'example/setExampleThunk',
+  async () => {
+    const example = await new Promise<Example>(resolve => setTimeout(() => resolve({text: 'createAsyncThunk'}), 3000)).then(value => value)
+    return example
+  }
+)
+
 const exampleSlice = createSlice({
   name: 'example',
   initialState,
@@ -16,6 +24,11 @@ const exampleSlice = createSlice({
     setExample: (state: Example, action: PayloadAction<Example['text']>) => {
       state.text = action.payload
     }
+  },
+  extraReducers: builder => {
+    builder.addCase(setExampleThunk.fulfilled, (state, action) => {
+      state.text = action.payload.text
+    })
   }
 })
 
