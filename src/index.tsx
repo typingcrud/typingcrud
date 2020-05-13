b4dd9394d7a230752998ffac653fe0e7d8335e8a
shimehituzi <shimehituzi@gmail.com>
2020-05-13 22:45:12 +0900

    redux init

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,12 @@
 import React from 'react'
 import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+import { store } from 'state'
 import App from 'views/App'
 
 render(
-  <App/>
+  <Provider store={store}>
+    <App/>
+  </Provider>
   , document.getElementById('root')
 )
