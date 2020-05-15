6125ce28c2e5571269d86124c3f12edfad878388
shimehituzi <shimehituzi@gmail.com>
2020-05-16 00:32:37 +0900

    signup thunk

---
+++ src/state/auth/signUpThunk.ts
@@ -0,0 +1,82 @@
+import { createAsyncThunk } from '@reduxjs/toolkit'
+import { CognitoUserAttribute, CognitoUser } from 'amazon-cognito-identity-js'
+
+import { cognitoUserPool } from 'utils/cognito/cognito-utils'
+import { AppState } from 'state'
+
+
+export const signUpThunk = createAsyncThunk<
+  void,
+  void,
+  {
+    state: AppState,
+  }
+  >(
+    'auth/signUpThunk',
+    async (_, thunkAPI) => {
+      const { isSignUpForm, ...signUpForm } = thunkAPI.getState().form.signUpForm
+      const attributeList = [
+        new CognitoUserAttribute({
+          Name: 'email',
+          Value: signUpForm.email
+        })
+      ]
+      cognitoUserPool.signUp(signUpForm.email, signUpForm.password, attributeList, [], (err) => {
+        if (err) {
+          alert(err.message || JSON.stringify(err))
+          return
+        }
+        alert("Success!")
+      })
+    }
+  )
+
+
+export const signUpVerifyThunk = createAsyncThunk<
+  void,
+  void,
+  {
+    state: AppState,
+  }
+  >(
+    'auth/signUpVerifyThunk',
+    async (_, thunkAPI) => {
+      const { isSignUpForm, ...signUpForm } = thunkAPI.getState().form.signUpForm
+      const cognitoUser = new CognitoUser({
+        Username: signUpForm.email,
+        Pool: cognitoUserPool
+      })
+      cognitoUser.confirmRegistration(signUpForm.verificationCode, true, (err: Error | undefined) => {
+        if (err) {
+          alert(err.message || JSON.stringify(err))
+          return
+        }
+        alert("Success!")
+      })
+    }
+  )
+
+
+export const signUpResendCodeThunk = createAsyncThunk<
+  void,
+  void,
+  {
+    state: AppState,
+  }
+  >(
+    'auth/signUpResendCodeThunk',
+    async (_, thunkAPI) => {
+      const { isSignUpForm, ...signUpForm } = thunkAPI.getState().form.signUpForm
+      const cognitoUser = new CognitoUser({
+        Username: signUpForm.email,
+        Pool: cognitoUserPool
+      })
+      cognitoUser.resendConfirmationCode((err) => {
+        if (err) {
+          alert(err.message || JSON.stringify(err))
+          return
+        }
+        alert("Success!")
+      })
+    }
+  )
