1b1d1a9bef542e2738de0767faecb651deafbc7f
k452 <55301839+k452@users.noreply.github.com>
2020-09-02 01:56:38 +0900

    gameに関する各種storeの設定

---
+++ src/state/game/gameForm/index.tsx
@@ -0,0 +1,32 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+
+import { submit } from 'state/game/gameForm/submit'
+
+type GameForm = {
+    title: string
+    description: string
+    code: string
+    codeComment: string
+}
+
+const initialState: GameForm = {
+    title: "",
+    description: "",
+    code: "",
+    codeComment: "",
+}
+
+const gameFormSlice = createSlice({
+    name: 'gameForm',
+    initialState,
+    reducers: {
+        reset: () => initialState,
+        changeForm: (_, action: PayloadAction<GameForm>) => action.payload
+    }
+})
+
+export const gameFormThunk = {
+    submit
+}
+
+export default gameFormSlice
