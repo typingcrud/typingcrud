6374451bded6b19825809fa8d34e9f562c06ab9e
shimehituzi <shimehituzi@gmail.com>
2020-09-03 06:48:19 +0900

    コンポーネントを Views にまとめた

---
+++ src/views/index.tsx
@@ -0,0 +1,31 @@
+import Home from 'views/Home'
+import NavBar from 'views/NavBar'
+import SignIn from 'views/SignIn'
+import SignUp from 'views/SignUp'
+import ForgotPassWord from 'views/ForgotPassWord'
+import Setting from 'views/Setting'
+import Demo from 'views/Demo'
+import Game from 'views/Game'
+import GameForm from 'views/Game/GameForm'
+import GameList from 'views/Game/GameList'
+import GamePlay from 'views/Game/GamePlay'
+import GameSetting from 'views/Game/GameSetting'
+import Terms from 'views/Terms'
+import Inquiry from 'views/Inquiry'
+
+export default {
+  Home,
+  NavBar,
+  SignIn,
+  SignUp,
+  ForgotPassWord,
+  Setting,
+  Demo,
+  Game,
+  GameForm,
+  GameList,
+  GamePlay,
+  GameSetting,
+  Terms,
+  Inquiry,
+}
