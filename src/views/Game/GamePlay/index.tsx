1efbc9db9f14f03651c245d5d059a685943d3c37
shimehituzi <shimehituzi@gmail.com>
2020-09-03 07:28:06 +0900

    modified

--- src/views/Game/GamePlay/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const GamePlay: React.FC = () => {
-    return (
-        <div></div>
-    )
-}
-
-export default GamePlay
--- src/views/Game/GameSetting/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const GameSetting: React.FC = () => {
-    return (
-        <div></div>
-    )
-}
-
-export default GameSetting
