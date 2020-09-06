71e636110159c2458f76452a4b867bfab1c84f0b
k452 <docomo9746@gmail.com>
2020-09-06 22:50:18 +0900

    インデント幅を2に変更

--- src/state/auth/updateTokens.ts
+++ src/state/auth/updateTokens.ts
@@ -7,35 +7,34 @@ import { actions } from 'state'
 
 
 export const updateTokens = createAsyncThunk<void, void, ThunkAPI>(
-    'auth/updateTokens',
-    async (_, thunkAPI) => {
-        const { isSignIn, tokens } = thunkAPI.getState().auth
-        if (!isSignIn && !tokens) {
-            return
-        }
-        const cognitoUser = cognitoUserPool.getCurrentUser()
-        if (cognitoUser === null) {
-            return
-        }
-        const rToken = new CognitoRefreshToken({ RefreshToken: localStorage.RefreshToken })
-        cognitoUser.refreshSession(rToken, (err, session) => {
-            if (err) {
-                console.log("トークンの更新に失敗しました。↓がエラー結果です")
-                console.error(err);
-            } else {
-                const idToken = session.getIdToken().getJwtToken()
-                const accessToken = session.getAccessToken().getJwtToken()
-                const refreshToken = session.getRefreshToken().getToken()
-                thunkAPI.dispatch(actions.auth.setTokens({
-                    idToken: idToken,
-                    accessToken: accessToken,
-                    refreshToken: refreshToken,
-                }))
-
-                localStorage.setItem('idToken', idToken)
-                localStorage.setItem('accessToken', accessToken)
-                localStorage.setItem('refreshToken', refreshToken)
-            }
-        })
+  'auth/updateTokens',
+  async (_, thunkAPI) => {
+    const { isSignIn, tokens } = thunkAPI.getState().auth
+    if (!isSignIn && !tokens) {
+      return
     }
+    const cognitoUser = cognitoUserPool.getCurrentUser()
+    if (cognitoUser === null) {
+      return
+    }
+    const oldToken = new CognitoRefreshToken({ RefreshToken: localStorage.RefreshToken })
+    cognitoUser.refreshSession(oldToken, (err, session) => {
+      if (err) {
+        console.log("トークンの更新に失敗しました。↓がエラー結果です")
+        console.error(err);
+      } else {
+        const idToken = session.getIdToken().getJwtToken()
+        const accessToken = session.getAccessToken().getJwtToken()
+        const refreshToken = session.getRefreshToken().getToken()
+        thunkAPI.dispatch(actions.auth.setTokens({
+          idToken: idToken,
+          accessToken: accessToken,
+          refreshToken: refreshToken,
+        }))
+        localStorage.setItem('idToken', idToken)
+        localStorage.setItem('accessToken', accessToken)
+        localStorage.setItem('refreshToken', refreshToken)
+      }
+    })
+  }
 )
