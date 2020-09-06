525df6450240947f282c716e3d69e1ecaeb2daad
shimehituzi <shimehituzi@gmail.com>
2020-09-07 00:23:11 +0900

    implemented getGame

---
+++ src/state/game/gamePlay/index.ts
@@ -0,0 +1,55 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+import { getGame } from 'state/game/gamePlay/getGame'
+import { Game } from 'state/game/gameList'
+
+type Params = {
+  cursorPos: {
+    row: number
+    col: number
+  }
+  gameOver: boolean
+}
+
+type GamePlay = {
+  game: Omit<Game, "userId">
+  params: Params
+}
+
+const initialState: GamePlay = {
+  game: {
+    index: '',
+    title: '',
+    description: '',
+    code: '',
+    codeComment: '',
+    createAt: '',
+    updateAt: ''
+  },
+  params: {
+    cursorPos: {
+      row: 0,
+      col: 0,
+    },
+    gameOver: false
+  }
+}
+
+const gamePlaySlice = createSlice({
+  name: 'gamePlay',
+  initialState,
+  reducers: {
+    reset: () => initialState,
+  },
+  extraReducers: builder => {
+    builder.addCase(getGame.fulfilled, (state, action) => {
+      state.game = action.payload ? action.payload : state.game
+    })
+  }
+})
+
+export const gamePlayThunk = {
+  getGame,
+}
+
+export default gamePlaySlice
