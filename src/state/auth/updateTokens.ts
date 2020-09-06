60cb5b7ff93fc5ab47f6166c64f289f9f814b3a1
k452 <docomo9746@gmail.com>
2020-09-06 22:38:20 +0900

    トークン更新action作成

--- src/state/auth/updateTokens.ts
+++ src/state/auth/updateTokens.ts
@@ -1,5 +1,6 @@
 import { createAsyncThunk } from '@reduxjs/toolkit'
-import { CognitoUserSession } from 'amazon-cognito-identity-js'
+import { CognitoRefreshToken } from 'amazon-cognito-identity-js'
+import { cognitoUserPool } from 'utils/cognito/cognito-utils'
 
 import { ThunkAPI } from 'utils/thunk'
 import { actions } from 'state'
@@ -12,58 +13,29 @@ export const updateTokens = createAsyncThunk<void, void, ThunkAPI>(
         if (!isSignIn && !tokens) {
             return
         }
-        /*
-
-        https://github.com/typingcrud/typingcrud-old/commit/d45cf0bc2e71b940061c7e6450f72a6b322eef3c
-        ↑のコミットにトークン更新処理が書いてあるのでそれを参考に実装お願いします
-        セッションを引数で受け取って処理するのが良いかなと思う
-        ↓以降はestablishSessionからのコピペなんで適当
-        
-        const localStorageDatas = {
-            idToken: localStorage.getItem('idToken'),
-            accessToken: localStorage.getItem('accessToken'),
-            refreshToken: localStorage.getItem('refreshToken'),
-            userId: localStorage.getItem("userId")
+        const cognitoUser = cognitoUserPool.getCurrentUser()
+        if (cognitoUser === null) {
+            return
         }
-        if (
-            localStorageDatas.idToken !== null &&
-            localStorageDatas.accessToken !== null &&
-            localStorageDatas.refreshToken !== null &&
-            localStorageDatas.userId !== null
-        ) {
-            thunkAPI.dispatch(actions.auth.setCognitoUser(true))
-            thunkAPI.dispatch(actions.auth.setTokens({
-                idToken: localStorageDatas.idToken,
-                accessToken: localStorageDatas.accessToken,
-                refreshToken: localStorageDatas.refreshToken
-            }))
-            thunkAPI.dispatch(actions.auth.setUserId(localStorageDatas.userId))
-
-        } else {
-            cognitoUser.getSession((err: Error, session: CognitoUserSession) => {
-                if (err) {
-                    alert(err.message || JSON.stringify(err))
-                    return
-                }
+        const rToken = new CognitoRefreshToken({ RefreshToken: localStorage.RefreshToken })
+        cognitoUser.refreshSession(rToken, (err, session) => {
+            if (err) {
+                console.log("トークンの更新に失敗しました。↓がエラー結果です")
+                console.error(err);
+            } else {
                 const idToken = session.getIdToken().getJwtToken()
                 const accessToken = session.getAccessToken().getJwtToken()
                 const refreshToken = session.getRefreshToken().getToken()
-                const userId = session.getIdToken().payload['custom:typing_userID']
-
-                thunkAPI.dispatch(actions.auth.setCognitoUser(true))
                 thunkAPI.dispatch(actions.auth.setTokens({
                     idToken: idToken,
                     accessToken: accessToken,
                     refreshToken: refreshToken,
                 }))
-                thunkAPI.dispatch(actions.auth.setUserId(userId))
 
                 localStorage.setItem('idToken', idToken)
                 localStorage.setItem('accessToken', accessToken)
                 localStorage.setItem('refreshToken', refreshToken)
-                localStorage.setItem('userId', userId)
-            })
-        }
-        */
+            }
+        })
     }
 )
