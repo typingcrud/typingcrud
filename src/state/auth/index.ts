252cc986f8c4245dc9a6380ac267d6095505af77
k452 <docomo9746@gmail.com>
2020-09-21 04:23:01 +0900

    ログイン後にユーザー情報取得の雛形

--- src/state/auth/index.ts
+++ src/state/auth/index.ts
@@ -6,6 +6,7 @@ import { signIn } from 'state/auth/signIn'
 import { signOut } from 'state/auth/signOut'
 import { signUp, signUpVerify, signUpResendCode } from 'state/auth/signUp'
 import { forgotPassword, submitNewPassword } from 'state/auth/forgotPassword'
+import { getUserInfo } from 'state/auth/getUserInfo'
 
 
 type Auth = {
@@ -16,12 +17,28 @@ type Auth = {
     refreshToken: string
   } | null
   userId: string
+  userInfo: {
+    userName: string
+    createdAt: string
+    updatedAt: string
+    imgOwn: string
+    imgType: string
+    img64: string
+  }
 }
 
 const initialState: Auth = {
   isSignIn: false,
   tokens: null,
   userId: '',
+  userInfo: {
+    userName: '',
+    createdAt: '',
+    updatedAt: '',
+    imgOwn: '',
+    imgType: '',
+    img64: ''
+  }
 }
 
 const authSlice = createSlice({
@@ -32,6 +49,12 @@ const authSlice = createSlice({
       state.isSignIn = initialState.isSignIn
       state.tokens = initialState.tokens
       state.userId = initialState.userId
+      state.userInfo.userName = initialState.userInfo.userName
+      state.userInfo.createdAt = initialState.userInfo.createdAt
+      state.userInfo.updatedAt = initialState.userInfo.updatedAt
+      state.userInfo.imgOwn = initialState.userInfo.imgOwn
+      state.userInfo.imgType = initialState.userInfo.imgType
+      state.userInfo.img64 = initialState.userInfo.img64
     },
     setCognitoUser: (state: Auth, action: PayloadAction<Auth['isSignIn']>) => {
       state.isSignIn = action.payload
@@ -41,6 +64,9 @@ const authSlice = createSlice({
     },
     setUserId: (state: Auth, action: PayloadAction<Auth['userId']>) => {
       state.userId = action.payload
+    },
+    setUserInfo: (state: Auth, action: PayloadAction<Auth['userInfo']>) => {
+      state.userInfo = action.payload
     }
   }
 })
@@ -52,6 +78,7 @@ export const authThunk = {
   signOut,
   signUp, signUpVerify, signUpResendCode,
   forgotPassword, submitNewPassword,
+  getUserInfo
 }
 
 export default authSlice
