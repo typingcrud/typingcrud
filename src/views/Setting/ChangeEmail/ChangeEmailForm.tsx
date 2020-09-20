a809ee57a2c15cbab253c8348b5793305ec60b68
k452 <55301839+k452@users.noreply.github.com>
2020-09-21 04:55:13 +0900

    メアド変更の場合のUI整備

--- src/views/Setting/ChangeEmail/ChangeEmailForm.tsx
+++ src/views/Setting/ChangeEmail/ChangeEmailForm.tsx
@@ -2,9 +2,60 @@ import React, { useCallback } from 'react'
 
 import { actions, thunkActions, useAppSelector, useAppDispatch } from 'state'
 
+import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import Input from '@material-ui/core/Input';
+import InputLabel from '@material-ui/core/InputLabel';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import FormControl from '@material-ui/core/FormControl';
+import Visibility from '@material-ui/icons/Visibility';
+import VisibilityOff from '@material-ui/icons/VisibilityOff';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      display: 'flex',
+      flexWrap: 'wrap',
+    },
+    margin: {
+      margin: theme.spacing(1),
+    },
+    textField: {
+      width: '25ch',
+    },
+    form: {
+      marginRight: 10,
+      marginLeft: 10,
+      marginBottom: 20,
+      display: 'inline-block'
+    },
+    button: {
+      marginLeft: 10,
+      marginTop: 10,
+      display: 'inline-block'
+    },
+  }),
+);
+
+interface State {
+  showPassword: boolean;
+}
 
 export const ChangeEmailForm: React.FC = () => {
   const { isNewEmailForm, ...changeEmailForm } = useAppSelector(state => state.setting.changeEmailForm)
+  const classes = useStyles();
+  const [values, setValues] = React.useState<State>({
+    showPassword: false
+  });
+
+  const handleClickShowPassword = () => {
+    setValues({ ...values, showPassword: !values.showPassword });
+  };
+
+  const handleMouseDownPassword = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+  };
 
   const dispatch = useAppDispatch()
   type ChangeEmailForm = typeof changeEmailForm
@@ -23,21 +74,37 @@ export const ChangeEmailForm: React.FC = () => {
   
   return (
     <React.Fragment>
-      <input
-        type='text'
-        id='newEmail'
-        placeholder='new email'
-        value={changeEmailForm.newEmail}
-        onChange={changeForm(changeEmailForm)}
-      />
-      <input
-        type='text'
-        id='password'
-        placeholder='password'
-        value={changeEmailForm.password}
-        onChange={changeForm(changeEmailForm)}
-      />
-      <button onClick={changeEmail}>change email</button>
+      <FormControl className={classes.form}>
+        <InputLabel htmlFor="standard-adornment-email">メールアドレス</InputLabel>
+        <Input
+          id='newEmail'
+          value={changeEmailForm.newEmail}
+          onChange={changeForm(changeEmailForm)}
+        />
+      </FormControl>
+      <FormControl className={classes.form}>
+        <InputLabel htmlFor="standard-adornment-password">パスワード</InputLabel>
+        <Input
+          id='password'
+          type={values.showPassword ? 'text' : 'password'}
+          value={changeEmailForm.password}
+          onChange={changeForm(changeEmailForm)}
+          endAdornment={
+            <InputAdornment position="end">
+              <IconButton
+                aria-label="toggle password visibility"
+                onClick={handleClickShowPassword}
+                onMouseDown={handleMouseDownPassword}
+              >
+                {values.showPassword ? <Visibility /> : <VisibilityOff />}
+              </IconButton>
+            </InputAdornment>
+          }
+        />
+      </FormControl>
+      <Button className={classes.button} variant="outlined" color="primary" onClick={changeEmail}>
+        認証コードを送信する
+      </Button>
     </React.Fragment>
   )
 }
--- src/views/Setting/ChangeEmail/VerificationForm.tsx
+++ src/views/Setting/ChangeEmail/VerificationForm.tsx
@@ -2,10 +2,41 @@ import React, { useCallback } from 'react'
 
 import { actions, thunkActions, useAppSelector, useAppDispatch } from 'state'
 
+import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import Input from '@material-ui/core/Input';
+import InputLabel from '@material-ui/core/InputLabel';
+import FormControl from '@material-ui/core/FormControl';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      display: 'flex',
+      flexWrap: 'wrap',
+    },
+    margin: {
+      margin: theme.spacing(1),
+    },
+    textField: {
+      width: '25ch',
+    },
+    form: {
+      marginRight: 10,
+      marginLeft: 10,
+      marginBottom: 20,
+      display: 'inline-block'
+    },
+    button: {
+      marginLeft: 10,
+      marginTop: 10,
+      display: 'inline-block'
+    },
+  }),
+);
 
 export const VerificationForm: React.FC = () => {
   const { isNewEmailForm, ...changeEmailForm } = useAppSelector(state => state.setting.changeEmailForm)
-
+  const classes = useStyles();
   const dispatch = useAppDispatch()
   type ChangeEmailForm = typeof changeEmailForm
   const changeForm = useCallback(
@@ -23,14 +54,17 @@ export const VerificationForm: React.FC = () => {
   
   return (
     <React.Fragment>
-      <input
-        type='text'
-        id='verificationCode'
-        placeholder='verification code'
-        value={changeEmailForm.verificationCode}
-        onChange={changeForm(changeEmailForm)}
-      />
-      <button onClick={verifyNewEmail}>verify</button>
+      <FormControl className={classes.form}>
+        <InputLabel htmlFor="standard-adornment-email">認証コード</InputLabel>
+        <Input
+          id='verificationCode'
+          value={changeEmailForm.verificationCode}
+          onChange={changeForm(changeEmailForm)}
+        />
+      </FormControl>
+      <Button className={classes.button} variant="outlined" color="primary" onClick={verifyNewEmail}>
+        確定
+      </Button>
     </React.Fragment>
   )
 }
