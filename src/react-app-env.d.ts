44bbce0d1d9202de3dfed9486171ce7f656d7989
k452 <55301839+k452@users.noreply.github.com>
2020-10-07 07:27:10 +0900

    本番環境のみga導入

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,13 @@
 /// <reference types="react-scripts" />
+interface Window {
+  gtag(type: 'config', googleAnalyticsId: string, { page_path: string })
+  gtag(
+    type: 'event',
+    eventAction: string,
+    fieldObject: {
+      event_label: string
+      event_category: string
+      value?: string
+    }
+  )
+} 
