3bcea3b9db44bccad029b8744d4e7bd248fcf83d
shimehituzi <shimehituzi@gmail.com>
2020-10-26 20:31:10 +0900

    ゲーム実装

--- src/views/App.tsx
+++ src/views/App.tsx
@@ -1,6 +1,7 @@
 import React, { useCallback, useEffect } from 'react'
-import { BrowserRouter, Route } from 'react-router-dom'
-import { createBrowserHistory } from 'history';
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import { createBrowserHistory } from 'history'
+import { Container } from '@material-ui/core'
 
 import { useAppDispatch, thunkActions } from 'state'
 import Views from 'views'
@@ -25,22 +26,26 @@ const App: React.FC = () => {
 
   return (
     <BrowserRouter>
-      <Route path='/'><Views.NavBar /></Route>
-      <Route exact path='/'><Views.Home /></Route>
-      <Route exact path='/user'><Views.Setting /></Route>
-      <Route path='/user/signin'><Views.SignIn /></Route>
-      <Route path='/user/signup'><Views.SignUp /></Route>
-      <Route path='/user/change-password'><Views.ChangePassWord /></Route>
-      <Route path='/user/forgot-password'><Views.ForgotPassWord /></Route>
-      <Route path='/user/change-email'><Views.ChangeEmailForm /></Route>
-      <Route path='/user/delete'><Views.UserSetting /></Route>
-      <Route path='/user/change-userinfo'><Views.ChangeUserInfo /></Route>
-      <Route path='/demo'><Views.Demo /></Route> {/*いずれ削除*/}
-      <Route exact path='/games'><Views.GameList/></Route>
-      <Route path='/games/:id'><Views.GamePlay/></Route>
-      <Route path='/games/new'><Views.GameNew/></Route>
-      <Route path='/terms'><Views.Terms /></Route> {/*規約ページ*/}
-      <Route path='/help'><Views.Help /></Route> {/*お問い合わせページ*/}
+      <Container>
+        <Route path='/'><Views.NavBar /></Route>
+        <Route exact path='/'><Views.Home /></Route>
+        <Route exact path='/user'><Views.Setting /></Route>
+        <Route path='/user/signin'><Views.SignIn /></Route>
+        <Route path='/user/signup'><Views.SignUp /></Route>
+        <Route path='/user/change-password'><Views.ChangePassWord /></Route>
+        <Route path='/user/forgot-password'><Views.ForgotPassWord /></Route>
+        <Route path='/user/change-email'><Views.ChangeEmailForm /></Route>
+        <Route path='/user/delete'><Views.UserSetting /></Route>
+        <Route path='/user/change-userinfo'><Views.ChangeUserInfo /></Route>
+        <Route path='/demo'><Views.Demo /></Route> {/*いずれ削除*/}
+        <Switch>
+          <Route exact path='/games'><Views.GameList /></Route>
+          <Route path='/games/new'><Views.GameNew /></Route>
+          <Route path='/games/:id'><Views.GamePlay /></Route>
+        </Switch>
+        <Route path='/terms'><Views.Terms /></Route> {/*規約ページ*/}
+        <Route path='/help'><Views.Help /></Route> {/*お問い合わせページ*/}
+      </Container>
     </BrowserRouter>
   )
 }
--- src/views/Game/Play/index.tsx
+++ src/views/Game/Play/index.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react'
 import { useParams } from 'react-router-dom'
-import { Typography, makeStyles, Grid, Container } from '@material-ui/core'
+import { Typography, makeStyles, Grid, Container, Paper } from '@material-ui/core'
 import { useAppSelector, useAppDispatch, actions, thunkActions } from 'state'
 import { Code } from './Code'
 import { Comment } from './Comment'
@@ -19,30 +19,32 @@ const GamePlay: React.FC = () => {
 
   const dispatch = useAppDispatch()
   useEffect(() => {
-    dispatch(thunkActions.gamePlay.getGame({index}))
+    dispatch(thunkActions.gamePlay.getGame({ index }))
 
     return () => { dispatch(actions.gamePlay.reset()) }
   }, [dispatch, index])
 
   return (
-    <Grid container justify='space-between' alignItems='center'>
-      <Grid item xs={9} sm={5}>
-        <Typography variant='h4' gutterBottom className={classes.typography}>{game.title}</Typography>
-      </Grid>
-      <Grid item xs={3}>
-        <Typography gutterBottom className={classes.typography}>{game.lang}</Typography>
-      </Grid>
-      <Container>
-        <Grid container justify='center' alignItems='flex-start' spacing={0}>
-          <Grid item xs={12} sm={6}>
-            <Comment />
-          </Grid>
-          <Grid item xs={12} sm={6}>
-            <Code />
-          </Grid>
+    <Paper elevation={10} square>
+      <Grid container justify='space-between' alignItems='center'>
+        <Grid item xs={9} sm={5}>
+          <Typography variant='h4' gutterBottom className={classes.typography}>{game.title}</Typography>
         </Grid>
-      </Container>
-    </Grid>
+        <Grid item xs={3}>
+          <Typography gutterBottom className={classes.typography}>{game.lang}</Typography>
+        </Grid>
+        <Container>
+          <Grid container justify='center' alignItems='flex-start' spacing={0}>
+            <Grid item xs={12} sm={6}>
+              <Comment />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <Code />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grid>
+    </Paper>
   )
 }
 
