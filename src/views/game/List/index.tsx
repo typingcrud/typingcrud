87bb46883a869475d9602c87ad18e5f1fbfaedb5
shimehituzi <51702091+shimehituzi@users.noreply.github.com>
2020-11-20 01:51:43 +0900

    Add ボタンを追加

--- src/views/game/List/index.tsx
+++ src/views/game/List/index.tsx
@@ -1,8 +1,10 @@
 import React, { useEffect } from 'react'
 import { useAppSelector, useAppDispatch, thunkActions } from 'state'
 import { CardElem } from './CardElem'
-import { Grid, Typography, makeStyles, colors, Paper } from '@material-ui/core'
+import { Grid, Typography, makeStyles, colors, Paper, Fab } from '@material-ui/core'
 import { useSignIn } from 'utils'
+import { Add } from '@material-ui/icons'
+import { useHistory } from 'react-router-dom'
 
 const useStyles = makeStyles({
   title: {
@@ -13,15 +15,23 @@ const useStyles = makeStyles({
     fontSize: 12,
     margin: 12,
     color: colors.grey[600],
+  },
+  add: {
+    margin: '1%'
   }
 })
 
 const GameList: React.FC = () => {
+  const history = useHistory()
   const gameList = useAppSelector(state => state.gameList)
   const signIn = useSignIn()
   const classes = useStyles()
 
   const dispatch = useAppDispatch()
+
+  const link = (path: string) => () => {
+    history.push(path)
+  }
 
   useEffect(() => {
     if (signIn) dispatch(thunkActions.gameList.getGames())
@@ -29,6 +39,11 @@ const GameList: React.FC = () => {
 
   return (
     <Paper elevation={10} square>
+      <Grid container justify='center'>
+        <Fab className={classes.add} color='secondary' size='small' onClick={link('/games/new')}>
+          <Add />
+        </Fab>
+      </Grid>
       <Grid container justify='center'>
         {gameList.map((game, index) => {
           return (
