3cc807561301e481017565028f03651456181e27
shimehituzi <51702091+shimehituzi@users.noreply.github.com>
2020-11-21 05:15:50 +0900

    SignIn の状態に応じてルーティングを行う & NotFound 処理

--- src/views/App.tsx
+++ src/views/App.tsx
@@ -4,6 +4,7 @@ import { createBrowserHistory } from 'history'
 import { Container } from '@material-ui/core'
 
 import { useAppDispatch, thunkActions } from 'state'
+import { useSignIn } from 'utils'
 import Views from 'views'
 
 const App: React.FC = () => {
@@ -12,6 +13,8 @@ const App: React.FC = () => {
   const establishSession = useCallback(
     () => dispatch(thunkActions.auth.establishSession()), [dispatch]
   )
+
+  const singIn = useSignIn()
 
   useEffect(() => {
     if (process.env.REACT_APP_TRACKING_ID) {
@@ -33,17 +36,18 @@ const App: React.FC = () => {
           <Route path='/signin'><Views.SignIn /></Route>
           <Route path='/signup'><Views.SignUp /></Route>
           <Route path='/forgot-password'><Views.ForgotPassWord /></Route>
-          <Route exact path='/user'><Views.Setting /></Route>
-          <Route path='/user/change-userinfo'><Views.ChangeUserInfo /></Route>
-          <Route path='/user/change-email'><Views.ChangeEmailForm /></Route>
-          <Route path='/user/change-password'><Views.ChangePassWord /></Route>
-          <Route path='/user/delete'><Views.DeleteUser /></Route>
-          <Route exact path='/games'><Views.GameList /></Route>
-          <Route path='/games/new'><Views.GameNew /></Route>
-          <Route path='/games/edit/:id'><Views.GameEdit /></Route>
+          {singIn && <Route exact path='/user'><Views.Setting /></Route>}
+          {singIn && <Route path='/user/change-userinfo'><Views.ChangeUserInfo /></Route>}
+          {singIn && <Route path='/user/change-email'><Views.ChangeEmailForm /></Route>}
+          {singIn && <Route path='/user/change-password'><Views.ChangePassWord /></Route>}
+          {singIn && <Route path='/user/delete'><Views.DeleteUser /></Route>}
+          {singIn && <Route exact path='/games'><Views.GameList /></Route>}
+          {singIn && <Route path='/games/new'><Views.GameNew /></Route>}
+          {singIn && <Route path='/games/edit/:id'><Views.GameEdit /></Route>}
           <Route path='/games/:id'><Views.GamePlay /></Route>
           <Route path='/terms'><Views.Terms /></Route>
           <Route path='/help'><Views.Help /></Route>
+          <Route render={() => <Views.NotFound />} />
         </Switch>
       </Container>
     </BrowserRouter>
