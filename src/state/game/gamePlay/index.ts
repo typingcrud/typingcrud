33dc32d627ca28f48662c54d919fb25a23261afb
shimehituzi <51702091+shimehituzi@users.noreply.github.com>
2020-11-21 08:46:53 +0900

    存在しないゲームIDのゲームを遊ぼうとしたときにエラーメッセージを表示

--- src/state/game/gamePlay/index.ts
+++ src/state/game/gamePlay/index.ts
@@ -1,6 +1,7 @@
 import { createSlice, } from '@reduxjs/toolkit'
 
 import { getGame } from 'state/game/gamePlay/getGame'
+import { notFoundSample } from 'state/game/gamePlay/notFoundSample'
 
 type GamePlay = Omit<App.Game, "userId">
 
@@ -22,8 +23,8 @@ const gamePlaySlice = createSlice({
     reset: () => initialState,
   },
   extraReducers: builder => {
-    builder.addCase(getGame.fulfilled, (state, action) => (
-      action.payload ? action.payload : state
+    builder.addCase(getGame.fulfilled, (_, action) => (
+      action.payload ? action.payload : notFoundSample
     ))
   }
 })
